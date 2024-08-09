93eec2eec03a977b57a2400d8e374060a7f8d85d
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2024-08-08 19:05:12 -0700

    delete remnants of devforum notifier + persist lastnewoutfit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 const { WebhookClient } = require('discord.js')
+const fs = require('fs')
 
 //setup dotenv
 const dotenv = require('dotenv')
@@ -21,20 +22,18 @@ require('node:https').globalAgent = new HttpsAgent()
 
 const cookie = process.env.robloxCookie
 
-//15 = group mention
-//17 = watching first post
-const notificationTypes = [15, 17]
-
-//just the webhook url lol
 const discordWebhook = process.env.discordWebhook 
-
 const webhooks = []
 for (let webhook of discordWebhook.split('\n')) {
     webhooks.push(new WebhookClient({url: webhook}))
 }
 console.log(`setup ${webhooks.length} webhooks`)
 
-let lastNewOutfit = 0
+if (!fs.existsSync('lastNewOutfit.txt')) {
+    fs.writeFileSync('lastNewOutfit.txt','0')
+}
+
+let lastNewOutfit = parseInt(fs.readFileSync('lastNewOutfit.txt','utf8'))
 
 //this is a lot easier as we just check for new outfits that are bundles lol
 async function main() {
@@ -45,13 +44,19 @@ async function main() {
             const outfitjson = await outfitres.json()
 
             if (outfitjson.data) {
+                let lastNewOutfitChanged = false
                 for (let outfit of outfitjson.data.reverse()) {
                     if (outfit.name !== "Roblox Curated Outfit" && outfit.id > lastNewOutfit) {
                         lastNewOutfit = outfit.id
+                        lastNewOutfitChanged = true
                         for (let webhook of webhooks) {
                             webhook.send(`New Outfit!\nName: ${outfit.name}\nCostume Id: ${outfit.id}`)
                         }
                     }
+                }
+
+                if (lastNewOutfitChanged) {
+                    fs.writeFileSync('lastNewOutfit.txt',lastNewOutfit.toString())
                 }
             } else {
                 console.log('rate limit -_-')
